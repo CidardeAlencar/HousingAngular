909932315715d781d3c3dc7cf6411ab3a1157fc9
Cidar de Alencar <101343945+CidardeAlencar@users.noreply.github.com>
2024-05-21 17:22:04 -0400

    Interfaces,Interpolación de valores,enlace de propiedades,directivas,servicios de Angular y la inyección de dependencias,rutas

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,31 @@
+import { Component, inject } from '@angular/core';
+import { HousingService } from '../housing.service';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule,HousingLocationComponent],
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Find a city">
+        <button type="button" class="primary">Search</button>
+      </form>
+    </section>
+    <section class="results">
+      <app-housing-location *ngFor="let housingLocation of housingLocationList" [housingLocation]="housingLocation"></app-housing-location>
+    </section>
+  `,
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  readonly baseUrl='https://angular.io/assets/images/tutorials/faa';
+  housingLocationList:HousingLocation[]=[];
+  housingService: HousingService = inject(HousingService);
+
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
+}
